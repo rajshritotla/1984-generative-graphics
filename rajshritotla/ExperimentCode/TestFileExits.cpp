0c551c9f031bc24b1c966d649475f3de3ef64c78
Rajshri Totla <rajshreetotla@gmail.com>
2019-11-16 17:45:01 -0500

    Test whether given file exists or not (additional C++ libraries required)

---
+++ rajshritotla/ExperimentCode/TestFileExits.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <ghc/filesystem.hpp>
+namespace fs = ghc::filesystem;
+using namespace std;
+
+
+int main () {
+
+    cout << fs::exists("hello.txt");
+    cout<<endl;
+    cout << fs::exists("000000000003_keypoints.json");
+    return 0;
+}
