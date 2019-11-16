3476e925df36989f923d6774fbf96af6f74973fe
Rajshri Totla <rajshreetotla@gmail.com>
2019-11-16 18:01:16 -0500

    extended testing for mac and linux along with windows

--- rajshritotla/ExperimentCode/TestCommandLineReadRun.cpp
+++ rajshritotla/ExperimentCode/TestCommandLineReadRun.cpp
@@ -7,17 +7,20 @@ using namespace std;
 // CHANGE THE PATH AND COMMAND AS PER APPLCATION LOCATION AND OPERATING SYSTEM
 
 // This is for windows and the location is tempoarary adjusted as per contributor local machine
+
+// https://github.com/CMU-Perceptual-Computing-Lab/openpose/blob/master/doc/output.md
 void callCamera()
 {
     cout<<"";
+    // SET LOCATION OR PATH
     system("cd.. && cd.. && cd.. &&  cd openpose &&  OpenPoseDemo.exe --model_pose COCO -write_json output");
-    
 }
 
 int main()
 {
-    //calling the webcam instance
-    callCamera(); 
-    getch();
+    // calling the webcam instance
+    // callCamera(); 
+    
+    system("dir || ls");
     return 0; 
 }
